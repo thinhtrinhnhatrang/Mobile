146ac4a051220b8b36f144e02353d4e5a371bc12
Thịnh Trịnh <quocthinh119@gmail.com>
2022-05-12 19:59:32 +0700

    Start
    commit

---
+++ app/src/main/java/com/example/listtodo/HomeActivity.java
@@ -0,0 +1,25 @@
+package com.example.listtodo;
+
+import androidx.appcompat.app.AlertDialog;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.WindowManager;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+
+public class HomeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.activity_home);
+
+    }
+    }
